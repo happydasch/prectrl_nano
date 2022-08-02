9d8798786719cb7db26175136761d3bca51721b2
happydasch <daniel@vcard24.de>
2022-08-02 13:22:33 +0200

    .

--- src/prectrl_nano.cpp
+++ src/prectrl_nano.cpp
@@ -23,8 +23,9 @@
                                                       // duration for 1 pas pulse in ticks: µs/tick_interval
 #define TORQUE_MIN 330                                // min torque = 0 (1,5V -> needs to be calibrated)
 #define TORQUE_MAX 480                                // max torque (around 3,0V -> needs to be calibrated)
-#define TORQUE_MAX_NM 102                             // max torque in Nm -> 306 torque value readings starting from torque_low
 #define TORQUE_FACTOR_NM 0.33                         // Torque reading to Nm factor
+#define TORQUE_MAX_WATT 500                           // max torque in watt
+#define TORQUE_MAX_NM 50                              // max torque in nm
 #define THROTTLE_MIN 250                              // min throttle
 #define THROTTLE_MAX 750                              // max throttle
 #define PAS_PULSES 36                                 // number of pulses per revolution for PAS signal
@@ -356,30 +357,34 @@ void read_torque() {
  *
  */
 void set_point() {
-  // set target to a mix of current power input and average power input to maintain some speed
-  // instead of just using current power input which is not accurate enough
-  //
-  // example 1 (torque only in nm 1/3 current and 2/3 average)
-  //   torque_pid_set = (torque_avg * 0.67 + torque * 0.33) * TORQUE_FACTOR_NM;  // multiplied with 0.33 for nm
-  //
-  // example 2 (torque only 1/2 current and 1/2 average):
-  //   torque_pid_set = (torque_avg + torque)/2;
-  //
-  // for torque based pid set:
-  //torque_pid_in = (torque - torque_avg) * factor[level_current] * -1;
-  //
-  // to obtain more power the pid set value can be scaled by a factor
-  // torque and cadence based (power input)
-  //   torque_pid_set = (power_input_avg + power_input) / 2;
-  // the target is no power input (the prectrl will try to balance out power input)
-  // scaled by current support level
-  //   torque_pid_in = (power_input - power_input_avg) * factor[level_current] * -1;
+  /*
+  set target to a mix of current power input and average power input to maintain some speed
+  instead of just using current power input which is not accurate enough
 
-  torque_pid_set = power_input * pedaling;
-  torque_pid_in = power_input_avg * factor[level_current] * pedaling * -1;
+  example 1 (torque only in nm 1/3 current and 2/3 average):
+    torque_pid_set = (torque_avg * 0.67 + torque * 0.33) * TORQUE_FACTOR_NM;  // multiplied with 0.33 for nm
+
+  example 2 (torque only 1/2 current and 1/2 average):
+    torque_pid_set = (torque_avg + torque)/2;
+
+  for torque based pid set:
+    torque_pid_in = (torque - torque_avg) * factor[level_current] * -1;
+
+  to obtain more power the pid set value can be scaled by a factor
+  torque and cadence based (power input):
+    torque_pid_set = (power_input_avg + power_input) / 2;
+
+  the target is no power input (the prectrl will try to balance out power input)
+  scaled by current support level:
+    torque_pid_in = (power_input - power_input_avg) * factor[level_current] * -1;
+  */
+  double scaled_torque = map(torque, 0 , TORQUE_MAX_NM, 0, 1023);
+  double scaled_torque_avg = map(torque_avg, 0 , TORQUE_MAX_NM, 0, 1023);
+  torque_pid_set = scaled_torque * pedaling;
+  torque_pid_in = scaled_torque_avg * factor[level_current] * pedaling;
 
   // update pid if not pedaling or no torque is provided
-  if (!pedaling || torque_avg <= 0) {
+  if (!pedaling || !scaled_torque_avg) {
     pid_torque.ShrinkIntegral();
   }
   // reset pid if brake is pressed
@@ -495,6 +500,7 @@ void update_pwm_output() {
     timer1.pwm(PIN_O_PWM, pwm_output);
     pwm_output_prev = pwm_output;
   }
+
   if (pedaling) {
     // FIXME remove after testing
     // output only when pedaling
